f9c53d3813d749bf84526cc7d1bd83a38c9d2617
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-08-28 13:31:59 +0200

    feat: :sparkles: ATM implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { useState } from "react";
-
+import ATM from "./components/ATM/ATM";
 export default function App() {
+  const [showATM, setShowATM] = useState(true);
   return (
     <div className="App">
       <h1>ATM</h1>
-
+      <button onClick={() => setShowATM((prevState) => !prevState)}>
+        {showATM ? "Hide ATM" : "Show ATM"}
+      </button>
+      {showATM && <ATM></ATM>}
     </div>
   );
 }
